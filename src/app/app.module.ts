1665cbfe49caa8f17f8a767e49f598471b81df0a
Luciano Ferreira de Almeida <originalbady@hotmail.com>
2022-06-05 19:38:36 -0300

    componentes-luciano ferreira de almeida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AsideComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
